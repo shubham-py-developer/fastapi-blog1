9469cf7e99a8c83288a725ceb50ac88298c1bf5a
shubham-py-developer <shubhprajapati042@gmail.com>
2026-02-10 23:24:53 +0530

    Fix  small typo in SECRET_KEY variable name

--- Backend/core/security.py
+++ Backend/core/security.py
@@ -7,5 +7,5 @@ def create_access_token(data:dict, expires_delta: Optional[timedelta]=None):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
-    encode_jwt =  jwt.encode(to_encode, settings.SECTRET_KEY, algorithm=settings.ALGORITHM)
+    encode_jwt =  jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encode_jwt
